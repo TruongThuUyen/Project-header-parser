e0c1421124b88beb1af3ea7a0be5196fc5eda610
May0105 <88717349+May0105@users.noreply.github.com>
2025-10-05 14:13:44 +0700

    first commit

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  const ipAddress = req.socket.remoteAddress;
+  const language = req.headers['accept-language'];
+  const softwareHeader = req.headers['user-agent'];
+  res.json({ ipAddress: ipAddress, language: language, software: softwareHeader });
+});
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
